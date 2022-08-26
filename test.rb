ff889becdf4426750ceed7539e168d221bccc0af
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-08-26 12:32:04 -0600

    Perform tests to verify the solution

---
+++ test.rb
@@ -0,0 +1,18 @@
+require_relative './My-List.rb'
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? { |e| e < 5 }
+# true
+list.all? { |e| e > 5 }
+# false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# true
+list.any? { |e| e == 5 }
+# false
+
+# Test #filter
+list.filter(&:even?)
+# [2. 4]
